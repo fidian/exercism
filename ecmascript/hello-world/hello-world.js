238a9e7979877747668391ce1a410ffaeffc2342
Tyler Akins <fidian@rumkin.com>
2016-06-16 13:47:10 -0500

    Solution

--- ecmascript/hello-world/hello-world.js
+++ ecmascript/hello-world/hello-world.js
@@ -1,14 +1,17 @@
-//
-// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 class HelloWorld {
-  hello() {
-    //
-    // YOUR CODE GOES HERE
-    //
-  }
+    /**
+     * Greet someone.  If nobody is named, greet the entire world.
+     *
+     * @param {string} name Name for greeting
+     * @return {string} "Hello, ${name}!"
+     */
+    hello(name) {
+        if (!name) {
+            name = "World"
+        }
+
+        return `Hello, ${name}!`;
+    }
 }
 
 export default HelloWorld;
