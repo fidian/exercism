898930fa12b246264a079ab2d06aec9a4430c5ca
Tyler Akins <fidian@rumkin.com>
2016-06-16 13:30:16 -0500

    Adding jsdoc

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,9 +1,27 @@
 var Year;
 
+/**
+ * Make an object that can determine if a year is a leap year.
+ *
+ * @constructor
+ * @param {integer} year
+ */
 Year = function (year) {
     this.year = +year;
 };
 
+
+/**
+ * Determine if the supplied year is a leap year.
+ *
+ * Multiple of 4?  Yes, except when ...
+ * multiple of 100?  No, except when ...
+ * multiple of 400?  Yes.
+ *
+ * This does not continue beyond 400-year adjustments.
+ *
+ * @return {boolean}
+ */
 Year.prototype.isLeap = function () {
     if (this.year % 4) {
         return false;
