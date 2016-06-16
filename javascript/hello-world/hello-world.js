36a5f740e79f2a8014c81369d6556fc15eeae353
Tyler Akins <fidian@rumkin.com>
2016-06-16 13:28:01 -0500

    Adding jsdoc

--- javascript/hello-world/hello-world.js
+++ javascript/hello-world/hello-world.js
@@ -1,11 +1,14 @@
-//
-// This is a stub file for the 'Hello World' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-// Make sure to look at hello-world.spec.js--that should give you some hints about what is
-// expected here.
+var HelloWorld;
 
-var HelloWorld = function () {};
+HelloWorld = function () {};
 
+/**
+ * Return a greeting to a specific person.  If no person is supplied,
+ * greet the entire world.
+ *
+ * @param {string} [input="World"]
+ * @return {string} "Hello, ${input}"
+ */
 HelloWorld.prototype.hello = function (input) {
     if (!input) {
         input = "World";
